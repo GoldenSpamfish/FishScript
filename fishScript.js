44750d91fe5bd87c19f92de35a427f6f9749bdd8
GoldenSpamfish <ryanellis3@gmail.com>
2021-01-29 10:23:23 -0500

    works now!

--- fishScript.js
+++ fishScript.js
@@ -31,33 +31,76 @@ let code = '>? What a cool language am I right?\n' +
     '    >$ print @\n' +
     '    >* repeat 4 print @ knowledge is power';
 lines = code.split('>');
-data = [];
+
 variables = {};
 for (let i = 0; i < lines.length; i++) {
     while (lines[i].includes('\n')) {
         lines[i] = lines[i].replace('\n', '');
     }
 }
-console.log(lines)
+//console.log(lines)
 
 // The print function
 function fishPrint(dataIn) {
     if (dataIn[0] === "@") {
         dataIn.shift()
-        return " ".join(dataIn)
+        return " ".concat(dataIn.join(" "))
     } else if (dataIn[0] === "&") {
         // meat of the print function (change to get element)
-        console.log(dataIn)
+        //console.log(dataIn)
 
         return variables[dataIn[1]]
     }
 }
 
 function variableDef(dataIn) {
-    if (dataIn.pop() === "number") {
-        delete dataIn[1]
-        variables[dataIn[0]] = eval(dataIn[1])
+    if (dataIn.shift() === 'number') {
+        variables[dataIn[0]] = eval(dataIn[2])
     }
+
 
 }
 
+//data = [];
+
+// ACTUAL RUNNER
+
+for (let line in lines) {
+    let data = lines[line].split(" ")
+    //console.log(lines[line])
+    //variable definition
+    if (data[0] === "&") {
+        data.shift();
+        variableDef(data);
+    }
+
+    //for running functions
+    else if (data[0] === "$") {
+        data.shift()
+
+        //print function
+        if (data.shift() === "print") {
+            console.log(fishPrint(data))
+        }
+    }
+
+    //loops
+    else if (data[0] === "*") {
+        data.shift()
+
+        //for repeat loops(effectively for loops)
+        if (data.shift() === "repeat") {
+            let repeats = eval(data.shift())
+
+
+            //repeated printing
+            if (data.shift() === "print") {
+                let toPrint = fishPrint(data)
+
+                for (let i = 0; i < repeats; i++) {
+                    console.log(toPrint)
+                }
+            }
+        }
+    }
+}
